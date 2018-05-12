5d43b37ff6229d42ff50d6129f1fa5a5b93622c8
zainfikrih <useryonoyou@gmail.com>
2018-05-12 16:42:10 +0700

    :boom: update

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -6,10 +6,16 @@ export default class componentName extends Component {
   render() {
     return (
       <div>
-        <div style={{ textAlign: 'center', lineHeight: '100vh', }} >
-          <Link to="/order/rooms">
-            <Button label='Booking Now' primary raised />
-          </Link>
+        <div style={{ textAlign: 'center', display: 'flex', flexDirection: 'column', paddingTop: '20vh' }} >
+          <div>
+            <img src='/static/img/logo.png' />
+          </div>
+          <h1>Brawijaya Hotel</h1>
+          <div style={{ paddingTop: '10vh' }} >
+            <Link to="/order/rooms">
+              <Button label='Booking Now' primary raised />
+            </Link>
+          </div>
         </div>
       </div>
     )
