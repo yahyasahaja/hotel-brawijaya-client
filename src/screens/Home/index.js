ca32b78c657dc972112229819b3f560a53861638
zainfikrih <useryonoyou@gmail.com>
2018-05-12 16:30:12 +0700

    :boom: sedikit aja

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import { Button } from 'react-toolbox/lib/button'
 
 export default class componentName extends Component {
   render() {
     return (
       <div>
-        <Link to="/order/rooms">Order</Link>
+        <div style={{ textAlign: 'center', lineHeight: '100vh', }} >
+          <Link to="/order/rooms">
+            <Button label='Booking Now' primary raised />
+          </Link>
+        </div>
       </div>
     )
   }
