9c0a04bcaa9b2e547f0ca9368eb3407c76b1bced
zainfikrih <useryonoyou@gmail.com>
2018-05-12 16:14:35 +0700

    :boom: add payment

--- src/screens/Payment/index.js
+++ src/screens/Payment/index.js
@@ -8,6 +8,12 @@ import Popup, { ANIMATE_HORIZONTAL } from '../../components/Popup'
 import ButtonBottom from '../../components/ButtonBottom'
 
 export default class Payment extends Component {
+  state = {
+    accountNumber: '165 524 789 26',
+    accountName: 'Brawijaya Hotel',
+    totalPrice: '800.000'
+  }
+  
   render() {
     return (
       <div>
@@ -16,10 +22,29 @@ export default class Payment extends Component {
           backLink="/order/checkout"
           anim={ANIMATE_HORIZONTAL}
         >
+          <div className={styles.container} >
+            <h3>Please transfer to the following <br/>bank account number.</h3>
+            <div className={styles.bank} >
+              <img src='/static/img/mandiri.PNG' />
+              <div>
+                {this.state.accountNumber}<br/>
+                {this.state.accountName}
+              </div>
+            </div>
+            <div className={styles.countdown} >
+              <h1>00 : 29 : 59</h1>
+            </div>
+            <div className={styles.price} >
+              Total Price
+              <div className={styles.totalPrice}>
+                Rp. {this.state.totalPrice}
+              </div>
+            </div>
+          </div>
 
           <ButtonBottom 
             link="/order/invoice"
-            name="GO"
+            name="WAITING"
           />
         </Popup>
       </div>
